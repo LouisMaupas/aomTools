db3997189dcf5a0739c0d78a8ebdaaa3b3b4a56f
LouisMaupas <louismaupas@gmail.com>
2024-09-10 17:46:22 +0200

    feat(Home): add shortcut

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
 import useStore from '../../store/store';
+import {Button} from "antd"
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const { userInfos } = useStore();
+  const navigate = useNavigate();
 
   return (
     <div>
       <h1>Bienvenue sur l'application !</h1>
-      {userInfos.civilization && userInfos.majorGod ? (
+      <div>
+              {userInfos.civilization && userInfos.majorGod ? (
         <div>
           <p>Votre civilisation favorite : {userInfos.civilization}</p>
           <p>Votre dieu majeur favori : {userInfos.majorGod}</p>
@@ -15,6 +18,10 @@ const Home = () => {
       ) : (
         <p>Vous n'avez pas encore sélectionné de civilisation et de dieu majeur.</p>
       )}
+      </div>
+      <div>
+        <Button onClick={()=> navigate("/counter-tool")} >Counters</Button>
+      </div>
     </div>
   );
 };
